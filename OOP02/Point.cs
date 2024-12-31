0d92d30b9a971bd820613dde7de367de29a6caaa
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-31 18:09:19 +0200

    Struct

---
+++ OOP02/Point.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP02
+{
+    internal struct Point
+    {
+        #region Attribute
+        //object member 
+        public int x;
+        public int y;
+        #endregion
+        ///Constructor :is a special function
+        ///           1-[take the same name of its class or struct]
+        ///           2-Has No Return Type
+        ///Constructor in Struct Can not be private
+        ///But in Constructor in class Access modifier can be any this from 6 [usually any thing except private ]
+        ///but in Singleton pattern 
+        ///Constructor use to initialization each and every attribute 
+        ///C#11 make a default constructor 
+        ///this refer to object i work on its
+        /// In Struct ,Complier Will Always Generate  Parameterless Constructor 
+        ///will initialize all attribute with the default value c#10 can write and update this[.net 6]
+
+        //can implement interface
+        #region Constructor
+        public Point()
+        {
+        }
+        public Point(int x,int y)
+        {
+            this.x= x; 
+            this.y = y;
+        }
+        public Point(int x)
+        {
+            this.x = x;
+            y = x;
+        }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return $"X: {x},Y: {y}";
+        }
+        #endregion
+
+
+
+    }
+}
--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Struct
+            ///stander for structure 
+            ///value type 
+            ///struct always use to  make data type 
+            ///class can by data type or just container[static class] all method like math class
+            ///size small like bool , int ,char 
+            ///data access faster than class because i was stack
+            ///Declare object with type point 
+            /// CLR will Allocate 8 bytes in stack
+            ///new with struct just choose constructor to initialize the attribute
+
+
+
+            //Point p01;
+            //p01.x = 5;
+            //p01.y = 3;
+            //Console.WriteLine(p01.x);
+            //Console.WriteLine(p01.y);
+            //p01=new Point(0,0)
+            //Console.WriteLine(P01.ToString());  
+            //Console.WriteLine(P01);
+            #endregion
         }
     }
 }
