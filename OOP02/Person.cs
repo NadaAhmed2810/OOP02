19cc2af6dcfd14eab606a8407d569a9bc22ccfd4
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-01 18:06:36 +0200

    Q1

---
+++ OOP02/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP02
+{
+    internal struct Person
+    {
+        #region Properties
+
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        #endregion
+        #region Constructors
+        public Person(String? Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return $"Name: {Name}\nAge:{Age}";
+        }
+        #endregion
+    }
+}
--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Part01
+            #region Q1:Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+            //Person[] people = new Person[3];
+            //people[0] = new Person("Nada", 20);
+            //people[1] = new Person("Noura", 20);
+            //people[2] = new Person("Nour", 19);
+            //Console.WriteLine("Data of all persons:");
+            //foreach (Person person in people)
+            //    Console.WriteLine(person.ToString());
+
+            #endregion
+
+            #endregion
         }
     }
 }
